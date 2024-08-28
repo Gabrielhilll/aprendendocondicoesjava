42d110ea4a70e0d0de830ea521daf550474c2806
Gabrielhilll <168039733+Gabrielhilll@users.noreply.github.com>
2024-08-28 19:22:44 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Integer idadePessoa = 20;
+        String NomePessoa = "Tainã";
+
+
+        /*
+         *Operadores logicos
+         * >= maior igual
+         * <= Menor igual
+         * == è igual
+         * equal: compara textos
+         * != diferente
+         */
+        if (idadePessoa < 18) {
+            System.out.println("é Menor de idade");
+        } else {
+            System.out.println("Não é maior");
+        }
+
+
+    }
+}
+
